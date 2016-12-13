1dba346a967188bc510c027c45f2589ee0f43d3e
Victor Fomenko <victorfomenko@me.com>
2016-12-13 17:04:58 +0300

    add ee render to component

--- src/UIFilters.js
+++ src/UIFilters.js
@@ -14,8 +14,16 @@ class UIFilters extends Component {
     super(props);
     this.state = {
       filters: this.props.filters || {}
+    };
+    this._initEEListeners();
+  }
+
+  _initEEListeners(){
+    if(typeof this.props.ee.on === 'function'){
+      this.props.ee.on('render', this.render.bind(this))
     }
   }
+
 
   _getStateObj () {
     var selected = {};
@@ -124,6 +132,7 @@ class UIFilters extends Component {
 }
 
 UIFilters.propTypes = {
+  ee: React.PropTypes.func,
   filters: React.PropTypes.object.isRequired,
   onStateChange: React.PropTypes.func.isRequired
 };
